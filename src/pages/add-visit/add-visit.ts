602bb439bbf6c5356dad740f681ae46b84a21a74
stizianel <stizianel@gmail.com>
2017-11-15 18:43:31 +0100

    con photo

--- src/pages/add-visit/add-visit.ts
+++ src/pages/add-visit/add-visit.ts
@@ -4,6 +4,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Customer } from '../../models/customer/customer.model';
 import { Visit } from '../../models/visit/visit.model';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { storage } from 'firebase';
 
 /**
  * Generated class for the AddVisitPage page.
@@ -19,6 +21,7 @@ import { Visit } from '../../models/visit/visit.model';
 })
 export class AddVisitPage {
   customer: Customer;
+  refKey: string = undefined;
 
   visit: Visit = {
     name: '',
@@ -30,7 +33,8 @@ export class AddVisitPage {
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
               private customerService: CustomerService,
-              private toast: ToastService) {
+              private toast: ToastService,
+              private camera: Camera) {
   }
 
   ionViewWillLoad() {
@@ -40,10 +44,34 @@ export class AddVisitPage {
 
   saveVisit(visit: Visit) {
     this.customerService.addCustomerVisit(visit, this.customer)
-      .then(() => {
+      .then((res) => {
+        this.refKey = res.key;
+        //debugger;
         this.toast.show(`${visit.name} saved!`);
-        this.navCtrl.setRoot('CustomerListPage');
+        //this.navCtrl.setRoot('CustomerListPage');
       });
   }
 
+  async takePhoto() {
+    try {
+      const options: CameraOptions = {
+        quality: 50,
+        targetHeight: 600,
+        targetWidth: 600,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE
+      }
+      const result = await this.camera.getPicture(options);
+
+      const image = `data:image/jpeg;base64,${result}`
+
+      const pictures = storage().ref('pictures');
+
+      pictures.putString(image, 'data_url');
+    }
+    catch (e) {
+      console.error(e);
+    }
+  }
 }
